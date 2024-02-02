384449bf5bf719267f91f289600e5df5d3a5caef
Rgelshteyn <raphaelgelshteyn@gmail.com>
2024-02-02 13:56:05 -0500

    fixed

--- Project4 (2)/Project4/ResturantReviewSystemProj3/Utilities/Connection.cs
+++ Project4 (2)/Project4/ResturantReviewSystemProj3/Utilities/Connection.cs
@@ -11,10 +11,10 @@ namespace Utilities
     public class DBConnect
     {
         // Main Connection String - used for the published web application and project submissions.
-        //String SqlConnectString = "server=cis-mssql1.temple.edu;Database=fa23_3342_tun33240;User id=tun33240;Password=Le7boob4fi";
+        //String SqlConnectString = "";
 
         // Home Connection String - used for working from home using SSH Tunneling.
-        String SqlConnectString = "server=127.0.0.1,5555;Database=fa23_3342_tun33240;User id=tun33240;Password=Le7boob4fi";
+        String SqlConnectString = "";
 
         SqlConnection myConnectionSql;
         SqlCommand objCmd;
